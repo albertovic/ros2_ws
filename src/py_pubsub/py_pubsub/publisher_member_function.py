806cee8b4f633770ec2c559ac4fb2615265d437d
albertovic <alberto.vicentechacon@gmail.com>
2023-11-23 13:40:35 +0100

    New pkg created

---
+++ src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -0,0 +1,9 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
