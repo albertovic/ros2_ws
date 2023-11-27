eda666782b52ad410875c66978fcb8155d5641ba
albertovic <alberto.vicentechacon@gmail.com>
2023-11-27 17:34:21 +0100

    Now the ros2 node works with arguments

--- src/py_pubsub/py_pubsub/subscriber_member_function.py
+++ src/py_pubsub/py_pubsub/subscriber_member_function.py
@@ -1,12 +1,14 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.logging import get_logger
 from std_msgs.msg import String
 import logging
+import argparse
+
 
 
 class MinimalSubscriber(Node):
-    def __init__(self):
+    #In this innit function, a path for the logger file is shared
+    def __init__(self, file_path):
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
             String,
@@ -15,17 +17,27 @@ class MinimalSubscriber(Node):
             10)
         self.subscription #prevent unussed variable warning
 
+        #We create a self.f so that other functions can use this variable, and open it in writing text mode
+        #This mode allows us to truncate the file when we open it
+        self.f = open(file_path, mode="wt")
+        print(file_path)
+
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
+        self.f.write(msg.data + "\n")
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-f', '--file_path', help='logger file name', default='file.log')
+    return arg_parser.parse_args()
+
 
 def main(args=None):
     rclpy.init(args=args)
+    parsed_args = parse_args()
+    logging_file_path = parsed_args.file_path
 
-    logging_file_path = 'file.log'
-
-    minimal_subscriber = MinimalSubscriber()
-    minimal_subscriber.get_logger().set_handler_by_name('file', filename=logging_file_path) #logging.INFO es para ajustar el nivel de registro
-    #puede ser logging.INFO, logging.WARNING, logging.ERROR, etc.    
+    minimal_subscriber = MinimalSubscriber(logging_file_path)
     rclpy.spin(minimal_subscriber)
 
     minimal_subscriber.destroy_node()
